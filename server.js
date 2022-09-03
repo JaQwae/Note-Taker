c77a9b81c4e141dd68fc340cb44d713377b151c6
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-03 13:07:47 -0500

    notes page html get request linked

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+// Data parsing middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Static Middleware
 app.use(express.static('public'));
 
-// GET request for the starting screen of the application
+
+// GET Route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+// GET Route for notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
 
 
 app.listen(PORT, () =>
