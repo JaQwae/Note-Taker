60fdae5a5cd7f410c72e3ae638d24d1e7700203f
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-03 14:06:55 -0500

    made landing page for the application to be deafault page

--- server.js
+++ server.js
@@ -13,17 +13,15 @@ app.use(express.json());
 // Static Middleware
 app.use(express.static('public'));
 
-
-// GET Route for homepage
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 // GET Route for notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// GET Route for homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
